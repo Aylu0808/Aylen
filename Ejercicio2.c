7aabca01d90f3fcf4fc49d13ebed11aa66bf75f3
Aylu0808 <105893167+Aylu0808@users.noreply.github.com>
2022-05-26 19:12:42 -0300

    Rename Ejercicio2.txt to Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+
+int main()
+{
+    
+int A[4];
+   
+    
+A[0] = 100;
+    
+A[1] = 200;
+    
+A[2] = 300;
+    
+A[3] = 400;
+
+    
+
+printf("%d %d %d %d \n", A[0], A[1], A[2], A[3]);
+    
+return 0;
+
+}
+
